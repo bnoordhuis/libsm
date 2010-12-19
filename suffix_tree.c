b021cc0704922817fba8623581fe0f62bb0033a2
Ben Noordhuis <info@bnoordhuis.nl>
2010-12-19 21:46:39 +0100

    Remove the concept of a leaf node, rely on NULL pointers.

--- suffix_tree.c
+++ suffix_tree.c
@@ -6,10 +6,7 @@
 
 typedef struct suffix_tree_node {
 	struct suffix_tree_node *next_sibling;
-
 	struct suffix_tree_node *first_child;
-	struct suffix_tree_node *last_child;
-
 	suffix_tree_string suffix;
 } suffix_tree_node;
 
@@ -17,13 +14,9 @@ struct suffix_tree {
 	suffix_tree_node *root;
 };
 
-static suffix_tree_node the_leaf_node = { NULL, NULL, NULL, { "", 0 } };
-
 static suffix_tree_node *new_suffix_tree_node(const char *str, int len);
 
 static void destroy_suffix_tree_node(suffix_tree_node *node);
-
-static int is_leaf_node(suffix_tree_node *node);
 
 static int has_children(suffix_tree_node *node);
 
@@ -131,10 +124,6 @@ static void destroy_suffix_tree_node(suffix_tree_node *node) {
 		return;
 	}
 
-	if (is_leaf_node(node)) {
-		return;
-	}
-
 	destroy_suffix_tree_node(node->first_child);
 	destroy_suffix_tree_node(node->next_sibling);
 
@@ -148,9 +137,7 @@ static suffix_tree_node *new_suffix_tree_node(const char *str, int len) {
 
 	if (node != NULL) {
 		node->next_sibling = NULL;
-
-		node->first_child = &the_leaf_node;
-		node->last_child = &the_leaf_node;
+		node->first_child = NULL;
 
 		node->suffix.str = str;
 		node->suffix.len = len;
@@ -234,37 +221,15 @@ static int find_split_point(suffix_tree_string *haystack, suffix_tree_string *ne
 	return k;
 }
 
-static int is_leaf_node(suffix_tree_node *node) {
-	assert(node != NULL);
-
-	if (node == &the_leaf_node) {
-		assert(the_leaf_node.next_sibling == NULL);
-
-		assert(the_leaf_node.first_child == NULL);
-		assert(the_leaf_node.last_child == NULL);
-
-		assert(the_leaf_node.suffix.str == NULL);
-		assert(the_leaf_node.suffix.len == 0);
-	}
-
-	return node == &the_leaf_node;
-}
-
 static int has_children(suffix_tree_node *node) {
-	return !is_leaf_node(node->first_child);
+	return node->first_child != NULL;
 }
 
 static void add_child_node(suffix_tree_node *parent, suffix_tree_node *child) {
 	assert(parent != NULL);
 
-	assert(parent->first_child != NULL);
-	assert(parent->last_child != NULL);
-
 	assert(child != NULL);
 	assert(child->next_sibling == NULL);
-
-	assert(!is_leaf_node(parent));
-	assert(!is_leaf_node(child));
 
 	child->next_sibling = parent->first_child;
 
@@ -276,9 +241,6 @@ static int split_node(suffix_tree_node *parent, suffix_tree_node *child0, int of
 
 	assert(parent != NULL);
 	assert(child0 != NULL);
-
-	assert(!is_leaf_node(parent));
-	assert(!is_leaf_node(child0));
 
 	assert(offset > 0);
 	assert(offset < parent->suffix.len);
